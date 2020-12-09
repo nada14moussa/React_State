07451a00d7fd0b0e758bb1bbcffd51026c79edda
nada14moussa <nada14moussa@gmail.com>
2020-12-09 13:47:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  state = {
+    Person:{fullName:'Nada MOUSSA' ,bio:'Always learn new things',imgSrc:'/photo.jpg',profession:'Web developer'},
+    show:false,
+    count:0
+  };
+  getProfile=()=>{
+    this.setState({show:!this.state.show})
+  }  
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={this.getProfile}> {this.state.show?'Hide Profile':'Show Profile'} </button>
+      {this.state.show ?
+     (<div> <h1> {this.state.Person.fullName}</h1>
+       <h2>{this.state.Person.bio}</h2>
+       <h2>{this.state.Person.profession}</h2>
+       <img src={this.state.Person.imgSrc}/>
+      </div>): (<h3>Click the button to show Profile</h3>)}
+     <div > Time : {this.state.count} s</div>
     </div>
-  );
+  );} 
+ componentDidMount(){
+   setInterval(()=>{  
+   this.setState(prevState =>({count:prevState.count+1})) } ,1000)
+ }
 }
 
 export default App;
